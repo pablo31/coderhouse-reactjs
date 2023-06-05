d935bc437f20d178ebe87df52c3864e4efb7cf64
Pablo Fernandez <pablo.fernandez@xselltechnologies.com>
2023-06-05 19:52:02 -0300

    ItemQuantity added to product detail

--- src/components/catalogue/ItemDetail.jsx
+++ src/components/catalogue/ItemDetail.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom"
 import Mocks from '../../mocks'
 import { useState, useEffect } from 'react'
+import ItemQuantity from '../ItemQuantity'
 
 function Product(props) {
     const { id } = useParams()
@@ -33,7 +34,17 @@ function Product(props) {
             </div>
             <div className="row">
                 <div className="col">
+                    <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum perspiciatis similique provident illo distinctio possimus inventore eligendi quis neque minus illum deserunt reprehenderit, eum itaque iste pariatur quae nulla quidem.</span>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
                     <span>${product.price}</span>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <ItemQuantity product={product} />
                 </div>
             </div>
         </div>
